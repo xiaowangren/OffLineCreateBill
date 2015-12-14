b9b2f9e9f1711edd0890bf4a0ef87792c582c8b7
xiaowangren <wangren_0626@126.com>
2015-12-14 15:58:06 +0800

    first bill commit

---
+++ view/BillApp.view.js
@@ -0,0 +1,41 @@
+sap.ui.jsview("com.zhenergy.bill.view.BillApp", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf com.zhenergy.bill.view.BillApp
+	*/ 
+	getControllerName : function() {
+		return "com.zhenergy.bill.view.BillApp";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf com.zhenergy.bill.view.BillApp
+	*/ 
+	createContent : function(oController) {
+		this.app = new sap.m.App({});
+		//注册overLook页面
+	    var BillOverLookPage = sap.ui.view({
+			id: "idBillOverLookPage",
+			viewName: "com.zhenergy.bill.view.BillOverLookPage",
+			type: sap.ui.core.mvc.ViewType.XML
+		});
+		this.app.addPage(BillOverLookPage);
+		//注册初始化页面
+		var BillInitializationPage = sap.ui.view({
+			id: "idBillInitializationPage",
+			viewName: "com.zhenergy.bill.view.BillInitializationPage",
+			type: sap.ui.core.mvc.ViewType.XML
+		});
+		this.app.addPage(BillInitializationPage);
+		//注册详细信息页面
+		var BillCreateInfoPage = sap.ui.view({
+			id: "idBillCreateInfoPage",
+			viewName: "com.zhenergy.bill.view.BillCreateInfoPage",
+			type: sap.ui.core.mvc.ViewType.XML
+		});
+		this.app.addPage(BillCreateInfoPage);
+		return this.app;
+	}
+
+});
