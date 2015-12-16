6f4c9bcfc41325fe4983a471c8e5a6acd9f50e82
xiaowangren <wangren_0626@126.com>
2015-12-16 18:05:56 +0800

    ok

--- view/BillInitializationPage.controller.js
+++ view/BillInitializationPage.controller.js
@@ -52,16 +52,77 @@ sap.ui.controller("com.zhenergy.bill.view.BillInitializationPage", {
       jQuery.sap.require("sap.m.MessageBox");
 // 		sap.m.MessageBox.alert("ZHIXING");
         //检查必填输入
-        if(!this.getView().byId("idWerksSelect").getSelectedKey())
+        var idWerksSelect = this.getView().byId("idWerksSelect").getSelectedKey();
+        var idTicketSelect = this.getView().byId("idTicketSelect").getSelectedKey();
+        if(!idWerksSelect)
         {
             sap.m.MessageBox.alert("请选择工厂");
             return;
         }
-        if(!this.getView().byId("idTicketSelect").getSelectedKey()){
+        if(!idTicketSelect){
             sap.m.MessageBox.alert("请选择操作票类型");
             return;
         }
-        
-        sap.ui.getCore().byId("idBillApp").app.to("idBillCreateInfoPage");
+        //获取Storage中的数据
+        jQuery.sap.require("jquery.sap.storage");
+		var oStorage = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+		var oLocalModel = new sap.ui.model.json.JSONModel();
+		if (oStorage.get("ZPMOFFLINE_SRV.ZPMT00204")) {
+			var oData = oStorage.get("ZPMOFFLINE_SRV.ZPMT00204");
+			oLocalModel.setProperty("/ZhiBie",oData);
+		}
+		sap.ui.getCore().setModel(oLocalModel);
+		if (oStorage.get("ZPMOFFLINE_SRV.ZPMT00227")) {
+			var oData1 = oStorage.get("ZPMOFFLINE_SRV.ZPMT00227");
+			var aFilter = [];
+			for(var i=0;i<oData1.length;i++){
+			    if(oData1[i].Werks==idWerksSelect){
+			        aFilter.push(oData1[i]);
+			    }
+			}
+		    oLocalModel.setProperty("/yunXingQuYu",aFilter);
+		}
+		sap.ui.getCore().setModel(oLocalModel);
+		if (oStorage.get("ZPMOFFLINE_SRV.ZPMT00283")) {
+			var oData2 = oStorage.get("ZPMOFFLINE_SRV.ZPMT00283");
+			var aFilter2 = [];
+			for(var j=0;j<oData2.length;j++){
+			    if(oData2[j].Werks==idWerksSelect){
+			        aFilter2.push(oData2[j]);
+			    }
+			}
+			console.log(aFilter2);
+		    oLocalModel.setProperty("/banZu",aFilter2);
+		}
+		sap.ui.getCore().setModel(oLocalModel);
+		//填写部门
+		if (oStorage.get("ZPMOFFLINE_SRV.ZPMT00229")) {
+			var oData3 = oStorage.get("ZPMOFFLINE_SRV.ZPMT00229");
+			var aFilter3 = [];
+			for(var m=0;m<oData3.length;m++){
+			    if(oData3[m].Werks==idWerksSelect){
+			        aFilter3.push(oData3[m]);
+			    }
+			}
+		    oLocalModel.setProperty("/tianxieBuMen",aFilter3);
+		}
+		sap.ui.getCore().setModel(oLocalModel);
+		//机组配置表
+		if (oStorage.get("ZPMOFFLINE_SRV.ZPMV00005")) {
+			var oData4 = oStorage.get("ZPMOFFLINE_SRV.ZPMV00005");
+			var aFilter4 = [];
+			for(var n=0;n<oData4.length;n++){
+			    if(oData4[n].Werks==idWerksSelect){
+			        aFilter4.push(oData4[n]);
+			    }
+			}
+			console.log(aFilter4);
+		    oLocalModel.setProperty("/jiZu",aFilter4);
+		}
+		sap.ui.getCore().setModel(oLocalModel);
+        if(idTicketSelect=="DQ"){
+            sap.ui.getCore().byId("idBillApp").app.to("idBillCreateInfoPage");
+
+        }
     }
 });
