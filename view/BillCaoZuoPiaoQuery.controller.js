9106aaed94a6d506a93f36c39bdcdb07429b19f0
xiaowangren <wangren_0626@126.com>
2015-12-22 18:49:12 +0800

    OK

--- view/BillCaoZuoPiaoQuery.controller.js
+++ view/BillCaoZuoPiaoQuery.controller.js
@@ -3,31 +3,13 @@ sap.ui.controller("com.zhenergy.bill.view.BillCaoZuoPiaoQuery", {
         //获取页面数据
         var gongChangQuery = this.getView().byId("gongChangQuery").getValue();//工厂
         var caoZuoPiaoLeiXingQuery = this.getView().byId("caoZuoPiaoLeiXingQuery").getValue();//操作票类型
-        var caoZuoHaoBianHaoStart = this.getView().byId("caoZuoHaoBianHao1").getSelectedKey();//操作票号
-        var caoZuoHaoBianHaoEnd = this.getView().byId("caoZuoHaoBianHao2").getSelectedKey();//操作票号
         var zhuangTaiQueryStart = this.getView().byId("zhuangTaiQuery1").getSelectedKey();//状态
-        var zhuangTaiQueryEnd = this.getView().byId("zhuangTaiQuery2").getSelectedKey();//状态
         var tianXieBuMenQueryStart = this.getView().byId("tianXieBuMenQuery1").getSelectedKey();//填写部门
-        var tianXieBuMenQueryEnd = this.getView().byId("tianXieBuMenQuery2").getSelectedKey();//填写部门
         var zhuanYeQueryStart = this.getView().byId("zhuanYeQuery1").getSelectedKey();//专业
-        var zhuanYeQueryEnd = this.getView().byId("zhuanYeQuery2").getSelectedKey();//专业
-        var caoZuoLeiXingQueryStart = this.getView().byId("caoZuoLeiXingQuery1").getSelectedKey();//操作类型
-        var caoZuoLeiXingQueryEnd = this.getView().byId("caoZuoLeiXingQuery2").getSelectedKey();//操作类型
         var jiZuQueryStart = this.getView().byId("jiZuQuery1").getSelectedKey();//机组
-        var jiZuQueryEnd = this.getView().byId("jiZuQuery2").getSelectedKey();//机组
-        var zhiBieQueryStart = this.getView().byId("zhiBieQuery1").getSelectedKey();//值别
-        var zhiBieQueryEnd = this.getView().byId("zhiBieQuery2").getSelectedKey();//值别
         var banZuQueryStart = this.getView().byId("banZuQuery1").getSelectedKey();//班组
-        var banZuQueryEnd = this.getView().byId("banZuQuery2").getSelectedKey();//班组
-        var caoZuoXingZhiQueryStart = this.getView().byId("caoZuoXingZhiQuery1").getSelectedKey();//操作性质
-        var caoZuoXingZhiQueryEnd = this.getView().byId("caoZuoXingZhiQuery2").getSelectedKey();//操作性质
-        var yunXingQuYuQueryStart = this.getView().byId("yunXingQuYuQuery1").getSelectedKey();//运行区域
-        var yunXingQuYuQueryEnd = this.getView().byId("yunXingQuYuQuery2").getSelectedKey();//运行区域
         var kaiPiaoRenQuery = this.getView().byId("kaiPiaoRenQuery").getValue();//开票人
         var caoZuoRenWuQuery = this.getView().byId("caoZuoRenWuQuery").getValue();//操作任务
-        var buXianShiDianXingPiaoQuery = this.getView().byId("buXianShiDianXingPiaoQuery").getChecked();//不显示典型票
-        var buXianShiQuXiaoPiaoQuery = this.getView().byId("buXianShiQuXiaoPiaoQuery").getChecked();//不显示取消票
-        var buXianShiZuoFeiPiaoQuery = this.getView().byId("buXianShiZuoFeiPiaoQuery").getChecked();//不显示作废票
         console.log(gongChangQuery+"=="+caoZuoPiaoLeiXingQuery+"--"+caoZuoHaoBianHaoStart+"=="+caoZuoHaoBianHaoEnd);
         //获取本地的数据，进行查询
         var abc=["0001","0002","0003","0004","0005","0006"];
