93d0996f11424442915318a4b57bfc2c6482ab21
xiaowangren <wangren_0626@126.com>
2016-01-04 11:20:40 +0800

    ok

--- view/BillOverLookPage.controller.js
+++ view/BillOverLookPage.controller.js
@@ -337,7 +337,7 @@ sap.ui.controller("com.zhenergy.bill.view.BillOverLookPage", {
 		sap.ui.getCore().setModel(oLocalModelQuery4);
 	},
 	onQuChuUser:function(){
-	    return {Cuser:"zhang3",Iwerk:"2081"};
+	    return {Cuser:"",Iwerk:"2081"};
 	},
 	onInit: function() {
 	    this.onReadLogDate();
