fa3450f6b7457dea06969d268664b51b1c92a13f
xiaowangren <wangren_0626@126.com>
2016-01-25 15:03:02 +0800

    ok 2016/1/25 15:02

--- view/GongZuoPiaoBaseInfo.controller.js
+++ view/GongZuoPiaoBaseInfo.controller.js
@@ -99,5 +99,26 @@ sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoBaseInfo", {
                 } 
             }    
         }
-    }
+    },
+    onAddKKS: function(evt){
+        
+        //开始搜索KKS
+		com.zhenergy.bill.util.SearchKKS.open(
+			null,
+			jQuery.proxy(this.onFinishAddKKS, this)
+		);
+    },
+	onFinishAddKKS: function(oResult) {
+	    var TplnrId = this.getView().byId("TplnrId");
+	    /*
+    	    搜索完范围选中的KKS结果:
+            {
+            	"Tplnr": "2081-40HTF14AP001",
+            	"Pltxt": "#4D吸收塔再循环泵"
+            }
+            处理选中的KKS
+	    */
+        // console.log(oResult);
+        TplnrId.setValue(oResult.Tplnr);
+	}
 });
