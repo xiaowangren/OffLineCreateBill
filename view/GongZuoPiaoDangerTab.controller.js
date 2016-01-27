ff4556d86a3393417f516ea0ec95c5af879815a8
xiaowangren <wangren_0626@126.com>
2016-01-27 14:01:41 +0800

    ok 2016/1/27 14:01 å±é©ç¹ä¿®æ¹

--- view/GongZuoPiaoDangerTab.controller.js
+++ view/GongZuoPiaoDangerTab.controller.js
@@ -1,3 +1,4 @@
+jQuery.sap.require("jquery.sap.storage");
 sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoDangerTab", {
     onInit:function(){
         this.getView().byId("verticalLayOutHeight");
@@ -28,15 +29,16 @@ sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoDangerTab", {
         var DangnoNum = Dangno+1;
         var Danger = {Zfxlx:"",Dangno:DangnoNum+"",Dangsnot:"",Zztext:"",Zzremark:"",Zzpltxt:""};
         DangerTab.insert(index+1, Danger);
-        for(var k=index+2;k<DangerTab.length;k++){
-                if(DangerTab[k].Dangno!=""){
-                    var DangnoInt = parseInt(DangerTab[k].Dangno)+1;
-                    DangerTab[k].Dangno = DangnoInt+"";  
-                }
+        this.OnChangeDans();
+        // for(var k=index+2;k<DangerTab.length;k++){
+        //         if(DangerTab[k].Dangno!=""){
+        //             var DangnoInt = parseInt(DangerTab[k].Dangno)+1;
+        //             DangerTab[k].Dangno = DangnoInt+"";  
+        //         }
                 
             
-        }
-        sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoBaseInfo").onrefresh("idGongZuoPiaoFinalView", WorkModel);
+        // }
+        // sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoBaseInfo").onrefresh("idGongZuoPiaoFinalView", WorkModel);
     },
     onDeleteDangerous:function(oEvent){
         var index = sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoBaseInfo").onAddDaleteIndex(oEvent);
@@ -51,20 +53,20 @@ sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoDangerTab", {
             sap.m.MessageBox.alert("此行无法进行删除",{title: "提示"});
             return;
         }
-        if(DangerElement.Dangno!=""){
-            for(var k=0;k<DangerTab.length;k++){
-                if(k<=index){
-                    if(DangerTab[k].Dangno!=""){
-                        DangerTab[k].Dangno = parseInt(DangerTab[k].Dangno)+"";
-                    }
-                }else{
-                    if(DangerTab[k].Dangno!=""){
-                        var DangnoInt = parseInt(DangerTab[k].Dangno)-1;
-                        DangerTab[k].Dangno = DangnoInt+"";
-                    }
-                }    
-            }
-        }
+        // if(DangerElement.Dangno!=""){
+        //     for(var k=0;k<DangerTab.length;k++){
+        //         if(k<=index){
+        //             if(DangerTab[k].Dangno!=""){
+        //                 DangerTab[k].Dangno = parseInt(DangerTab[k].Dangno)+"";
+        //             }
+        //         }else{
+        //             if(DangerTab[k].Dangno!=""){
+        //                 var DangnoInt = parseInt(DangerTab[k].Dangno)-1;
+        //                 DangerTab[k].Dangno = DangnoInt+"";
+        //             }
+        //         }    
+        //     }
+        // }
         Array.prototype.baoremove = function(dx) 
         { 
             if(isNaN(dx)||dx>this.length){return false;} 
@@ -72,24 +74,77 @@ sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoDangerTab", {
         }
         
         DangerTab.baoremove(index);
-        sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoBaseInfo").onrefresh("idGongZuoPiaoFinalView", WorkModel);
+        this.OnChangeDans();
+        // sap.ui.controller("com.zhenergy.bill.view.GongZuoPiaoBaseInfo").onrefresh("idGongZuoPiaoFinalView", WorkModel);
     },
-    onChangeDangno:function(oEvent){
+    OnChangeDans: function(){
         var WorkModel = this.getView().getModel("WorkModel").getData(); 
         var DangerTab = WorkModel.DangerTab;
-        var sPath = oEvent.getSource().oParent.oBindingContexts.WorkModel.sPath;
+        //更新序号
+        var AQCSDict={};
+        for(var i=0;i<DangerTab.length;i++){
+            if(DangerTab[i].Zfxlx){
+                if(AQCSDict[DangerTab[i].Zfxlx]){
+                    AQCSDict[DangerTab[i].Zfxlx]++
+                }else{
+                    AQCSDict[DangerTab[i].Zfxlx]=1
+                }
+                DangerTab[i].Dangno=AQCSDict[DangerTab[i].Zfxlx]+"";
+            }
+
+        }
+        this.onrefresh("idGongZuoPiaoFinalView", WorkModel);
+    },
+    onAddDaleteIndex:function(oEvent){
+        var source = oEvent.getSource();
+        var sPath = source.oPropagatedProperties.oBindingContexts.WorkModel.sPath;
         var splits = sPath.split("/");
         var index = parseInt(splits[2]);
-        for(var k=0;k<DangerTab.length;k++){
-            if(k<=index&&DangerTab[k].Dangno!=""){
-                DangerTab[k].Dangno = parseInt(DangerTab[k].Dangno)+"";
-            }else{
-                if(DangerTab[k].Dangno!=""){
-                    var DangnoInt = parseInt(DangerTab[k].Dangno)-1;
-                    DangerTab[k].Dangno = DangnoInt+"";
-                } 
-            }    
+        return index;
+    },
+    onrefresh:function(pageId,model){
+        var oModel = new sap.ui.model.json.JSONModel(model);
+        sap.ui.getCore().byId("idBillApp").app.to(pageId, model);
+        var page = sap.ui.getCore().byId("idBillApp").app.getPage(pageId);
+    	page.setModel(oModel,"WorkModel"); 
+    },
+    onSelect:function(oEvent){
+        //获取index
+        var index = this.onAddDaleteIndex(oEvent);
+        var WorkModel = this.getView().getModel("WorkModel").getData(); 
+        var DangerTab = WorkModel.DangerTab;
+         
+        //更新序号
+        var AQCSDict={};
+        for(var i=0;i<DangerTab.length;i++){
+            if(DangerTab[i].Zfxlx){
+                if(AQCSDict[DangerTab[i].Zfxlx]){
+                    AQCSDict[DangerTab[i].Zfxlx]++
+                }else{
+                    AQCSDict[DangerTab[i].Zfxlx]=1
+                }
+                DangerTab[i].Dangno=AQCSDict[DangerTab[i].Zfxlx]+"";
+            }
+
         }
+        this.onrefresh("idGongZuoPiaoFinalView", WorkModel);
     }
+    // onChangeDangno:function(oEvent){
+    //     var WorkModel = this.getView().getModel("WorkModel").getData(); 
+    //     var DangerTab = WorkModel.DangerTab;
+    //     var sPath = oEvent.getSource().oParent.oBindingContexts.WorkModel.sPath;
+    //     var splits = sPath.split("/");
+    //     var index = parseInt(splits[2]);
+    //     for(var k=0;k<DangerTab.length;k++){
+    //         if(k<=index&&DangerTab[k].Dangno!=""){
+    //             DangerTab[k].Dangno = parseInt(DangerTab[k].Dangno)+"";
+    //         }else{
+    //             if(DangerTab[k].Dangno!=""){
+    //                 var DangnoInt = parseInt(DangerTab[k].Dangno)-1;
+    //                 DangerTab[k].Dangno = DangnoInt+"";
+    //             } 
+    //         }    
+    //     }
+    // }
 
 });
